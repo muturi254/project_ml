3cb10018a3494d572ebc22d9be2aca3626068300
muturi25 <petermuturi74@gmail.com>
2022-09-05 12:38:38 +0300

    add gitignore

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build -t ashorta/project_ml: latest . 
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+docker run --name prediction_app -it -p 5000:80 project_ml
